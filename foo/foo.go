6ebf56585850af7fd05a3ad888d311b214b4b9b2
Romalin99 <romalin99@gmail.com>
2020-04-10 16:37:04 +0800

    upgrade 1.0.0

--- foo/foo.go
+++ foo/foo.go
@@ -3,5 +3,5 @@ package foo
 import "fmt"
 
 func Foo() {
-	fmt.Println("foo.Foo of module: github.com/romalin99/-modules-major-branch2 pre-v1")
+	fmt.Println("foo.Foo of module: github.com/romalin99/-modules-major-branch2 v1.0.0")
 }
